167289c64b7933a2babcdf7f3bf082fbf02d0878
Sebastian Öberg <anders.sebastian.oberg@gmail.com>
2018-02-14 19:01:05 +0100

    Add functionality to pass intent to new activity

--- app/src/main/java/com/sebastianoberg/androidplayaround/MainActivity.java
+++ app/src/main/java/com/sebastianoberg/androidplayaround/MainActivity.java
@@ -1,13 +1,26 @@
 package com.sebastianoberg.androidplayaround;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MESSAGE_CONSTANT = "First message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(MESSAGE_CONSTANT, message);
+        startActivity(intent);
     }
 }
